863c88d8fa2d3357911f793f4cf60283911358dc
avanizeal <avanizeal.com>
2017-04-28 13:39:50 +0530

    jkkkk

--- app/src/main/java/com/example/ferooz/horticulture/Login.java
+++ app/src/main/java/com/example/ferooz/horticulture/Login.java
@@ -47,7 +47,7 @@ public class Login extends AppCompatActivity {
         }
 
 
-        newuser.setOnClickListener(new View.OnClickListener() {
+       /* newuser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent in=new Intent(Login.this,RegisterPage.class);
@@ -56,7 +56,7 @@ public class Login extends AppCompatActivity {
 
 
             }
-        });
+        });*/
 
       /*  login.setOnClickListener(new View.OnClickListener() {
             @Override
